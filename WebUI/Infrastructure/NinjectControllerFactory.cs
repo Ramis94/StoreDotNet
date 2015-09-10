26c737a40aa18b50e5e8e7eb0ce7235f881c89a1
ramis.gilmanov <Ramis94@yandex.ru>
2015-09-10 17:38:28 +0300

    Initial

---
+++ WebUI/Infrastructure/NinjectControllerFactory.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Ninject;
+using Domain.Entities;
+using Domain.Abstract;
+using System.Collections.Generic;
+using System.Linq;
+using Moq;
+using Domain.Concrete;
+using System.Configuration;
+using WebUI.Infrastructure.Abstract;
+using WebUI.Infrastructure.Concrete;
+
+namespace WebUI.Infrastructure
+{
+    // реализация пользовательской фабрики контроллеров,
+    // наследуясь от фабрики используемой по умолчанию
+    public class NinjectControllerFactory : DefaultControllerFactory
+    {
+        private IKernel ninjectKernel;
+        public NinjectControllerFactory()
+        {
+            // создание контейнера
+            ninjectKernel = new StandardKernel();
+            AddBindings();
+        }
+        protected override IController GetControllerInstance(RequestContext requestContext,
+        Type controllerType)
+        {
+            // получение объекта контроллера из контейнера
+            // используя его тип
+            return controllerType == null
+            ? null
+            : (IController)ninjectKernel.Get(controllerType);
+        }
+        private void AddBindings()
+        {
+            // конфигурирование контейнера
+            ninjectKernel.Bind<IProductRepository>().To<EFProductRepository>();
+            EmailSettings emailSettings = new EmailSettings
+            {
+                WriteAsFile = bool.Parse(ConfigurationManager
+                .AppSettings["Email.WriteAsFile"] ?? "false")
+            };
+            ninjectKernel.Bind<IOrderProcessor>()
+            .To<EmailOrderProcessor>()
+            .WithConstructorArgument("settings", emailSettings);
+            ninjectKernel.Bind<IAuthProvider>().To<FormsAuthProvider>();
+        }
+    }
+}
